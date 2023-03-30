def6a4a9bc3a93caa5de36e99cf275be516d2c5e
RoVenegasV <rociovenegasvivanco@gmail.com>
2023-03-29 22:28:29 -0300

    commit 2

---
+++ index.js
@@ -0,0 +1,23 @@
+/* Ejercicio 1 - Calcular el IMC*/
+
+var peso = 75;
+var estatura = 1.8;
+var imc;
+imc = peso/(estatura*estatura);
+
+console.log(imc);
+
+/* Ejercicio 2 - Calcular IVA*/
+
+var compra = 10000;
+const iva = 0.19;
+var compraIVA =  compra * iva;
+
+console.log(compraIVA);
+
+/* Ejercicio 3 - Calcular la hipotenusa dados los catetos*/
+var catop = 3;
+var catad = 4;
+var hipotenusa;
+hipotenusa = Math.sqrt((catop*catop) + (catad*catad)); 
+console.log(hipotenusa);
